d24d7629269b12ba1b8710232efad0bdd596a570
Andres Auais <andresauais@gmail.com>
2021-01-25 21:23:12 +0000

    more css

--- src/assets/scripts/views/components/containers/characters.js
+++ src/assets/scripts/views/components/containers/characters.js
@@ -1,14 +1,14 @@
 export const characters = ({name, image, status, species, origin, id}) => {
   const template = `
   <div class="carousel-item">
-    <div class="col-md-4 episode-card">
+    <div class="col-md-4">
       <div class="card card-body characters-card">
         <img class="card-img-top" src="${image}" alt="">
           <div class="card-body">
             <h4>${name}</h4>
             <p class="origin-info">${origin}</p>
             <p>${species} || ${status}</p>
-            <button class="btn btn-primary characters-card__button" id="${id}">More Info</button>
+            <button class="characters-card__button" id="${id}">More Info</button>
           </div>
       </div>
     </div>
